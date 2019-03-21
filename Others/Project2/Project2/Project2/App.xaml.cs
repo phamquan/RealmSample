46fab4445be85b2d5d5a853ccbc9c6f4c6726d83
Quan Pham (VN) <quan.p@homecredit.vn>
2019-03-21 11:30:47 +0700

    add project 2 for sample reuse module

---
+++ Others/Project2/Project2/Project2/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Biz4.RealmSample.ToDo;
+using Prism;
+using Prism.Ioc;
+using Prism.Modularity;
+using Project2.ViewModels;
+using Project2.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Project2
+{
+    public partial class App
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/ToDoListPage");
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+
+            moduleCatalog.AddModule<ToDo>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
